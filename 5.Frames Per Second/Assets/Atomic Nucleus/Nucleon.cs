cb023ae972ce2c5008aa7be592c3cf1a183118fe
heinrichzzl <heinrichzzl@163.com>
2020-10-20 18:40:50 +0200

    Frames Per Second

---
+++ 5.Frames Per Second/Assets/Atomic Nucleus/Nucleon.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Nucleon : MonoBehaviour
+{
+    public float attractionForce;
+
+    Rigidbody body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        body.AddForce(-transform.localPosition * attractionForce);
+    }
+
+
+}
---
+++ 5.Frames Per Second/Assets/Atomic Nucleus/NucleonSpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class NucleonSpawner : MonoBehaviour
+{
+    public float timeBetweenSpawns;
+
+    public float SpawnDistance;
+
+    public Nucleon[] nucleonsPreferbs;
+
+    float timeSinceLastSpawn;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn >= timeBetweenSpawns)
+        {
+            timeSinceLastSpawn -= timeBetweenSpawns;
+            SpawnNucleon();
+        }
+    }
+
+    void SpawnNucleon()
+    {
+        Nucleon nucleon = nucleonsPreferbs[Random.Range(0, nucleonsPreferbs.Length)];
+        Nucleon spawn = Instantiate<Nucleon>(nucleon);
+        spawn.transform.localPosition = Random.onUnitSphere * SpawnDistance;
+    }
+
+}
